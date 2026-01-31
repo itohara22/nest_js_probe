0944f51fa94e06f55a224be7ab6b0a83c6e79a58
shubham-ghogle <shubham.ghogle@innovyatech.com>
2026-01-31 23:50:55 +0530

    custom exceptions

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Cat } from './cat.interface';
 
 @Injectable()
@@ -6,6 +6,10 @@ export class CatsService {
   private readonly cats: Cat[] = [];
 
   create(cat: Cat) {
+    if (!cat.age || cat.age < 0) {
+      throw new BadRequestException('noooo'); // custom message
+    }
+
     this.cats.push(cat);
   }
 
