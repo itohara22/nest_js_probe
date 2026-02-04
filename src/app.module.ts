94cb5a485fd7a850c0e356717e64812ccdf819da
shubham-ghogle <shubham.ghogle@innovyatech.com>
2026-02-04 17:30:13 +0530

    registering authModule, orgModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
-
+import { AuthModule } from './auth/auth.module'
+import { OrgModule } from './organization/org.module'
+ 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, AuthModule, OrgModule],
   controllers: [AppController],
   providers: [AppService],
 })
